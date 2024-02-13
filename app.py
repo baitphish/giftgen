3d1ccc760cf62a1fe462c1f39e9d719b229c0f53
BaitPhish <109495248+baitphish@users.noreply.github.com>
2024-02-13 17:20:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,17 +7,21 @@ config = load_config('config.json')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    platforms = config["platforms"] + config["custom_stores"] 
-    return render_template('index.html', platforms=platforms)
+    platforms = config["platforms"] + config["custom_stores"]  
+
+    if request.method == 'POST':  
+         # Code generation will  go here (once we finish the results page)!
+
+    return render_template('index.html', platforms=platforms)  
+
 
 @app.route('/results', methods=['POST'])
 def results():
     platform = request.form['platform']
     num_codes = int(request.form['num_codes'])
-    # Add batch generation if you implement it!
-
-    codes = generate_codes.generate_gift_card_codes(config, num_codes)
-    return render_template('results.html', codes=codes)
+    codes = generate_codes.generate_gift_card_codes(config, num_codes)  
+    return render_template('results.html', codes=codes) 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
